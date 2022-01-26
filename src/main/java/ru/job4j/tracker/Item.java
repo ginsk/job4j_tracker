d4db89d629e5cc329e25da5c825c91c2cf8e416b
ginsk <gin2003@gmail.com>
2022-01-26 14:57:00 +0300

    Репозитoрий под проект Tracker

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,10 +1,19 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
 
     private int id;
 
     private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    public  LocalDateTime getCreated() {
+        return created;
+    }
 
     public Item() {
     }
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI extends Item {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String time = formatter.format(item.getCreated());
+        System.out.println(time);
+    }
+}
