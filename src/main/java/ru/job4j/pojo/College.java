23ef6003d873820688f9aba2e909de25c57e7212
ginsk <gin2003@gmail.com>
2022-01-27 17:27:55 +0300

    Репозитoрий под проект Tracker

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -2,13 +2,13 @@ package ru.job4j.pojo;
 
 import java.util.Date;
 
-public class College extends Student {
+public class College  {
     public static void main(String[] args) {
-        College college = new College();
-        college.setFoolName("Саша Грей");
-        college.setDate("14.02.2020");
-        college.setGroup("cleanLove");
-        System.out.println(college.getFoolName() +  " преподаст " + college.getDate() + " урок "
-                + college.getGroup());
+        Student student = new Student();
+        student.setFoolName("Саша Грей");
+        student.setDate("14.02.2020");
+        student.setGroup("cleanLove");
+        System.out.println(student.getFoolName() +  " преподаст " + student.getDate() + " урок "
+                + student.getGroup());
     }
 }
