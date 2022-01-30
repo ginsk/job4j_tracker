9dbe1dbc5ba10e7814679d139da1bfa4ddf0c7b5
ginsk <gin2003@gmail.com>
2022-01-30 12:36:36 +0300

    Репозитoрий под проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -75,4 +75,14 @@ public class Tracker {
         items[index] = item;
         return rsl;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        int start = index + 1;
+        int length = size - index - 1;
+        items[size - 1] = null;
+        size--;
+        System.arraycopy(items, start, items, index, length);
+        return true;
+    }
 }
