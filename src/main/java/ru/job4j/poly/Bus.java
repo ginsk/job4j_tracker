84d8d2895cc1e4172cbf889358be38601bfdc177
ginsk <gin2003@gmail.com>
2022-02-03 13:30:29 +0300

    Репозитoрий под проект Tracker

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,12 +3,12 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void trip() {
-
+        System.out.println("Один плацкарт до Жмеринки!");
     }
 
     @Override
     public void passenger(int ticket) {
-
+        System.out.println("Карбованци треба!");
     }
 
     @Override
