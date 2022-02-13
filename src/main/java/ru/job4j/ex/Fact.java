d37dda49b51b62c03a1f5cdf7bde67decb8a42e1
ginsk <gin2003@gmail.com>
2022-02-13 18:09:12 +0300

    Репозитoрий под проект Tracker

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -1,12 +1,8 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public static void main(String[] args) {
-        new Fact().calc(-1);
 
-    }
-
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n > 0) {
             throw new IllegalArgumentException("n should be less than 0.");
         }
