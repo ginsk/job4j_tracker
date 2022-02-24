f82eece8625210bc6d47f22d2c6190b01beab5c1
ginsk <gin2003@gmail.com>
2022-02-24 08:44:46 +0300

    Репозитoрий под проект Tracker

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,35 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+
+public final class SingleTracker {
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker () {
+
+    }
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id,item);
+    }
+    public boolean delete(int id) {
+       return tracker.delete(id);
+    }
+    }
+
+
+
